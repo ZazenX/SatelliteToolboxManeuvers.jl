5caea80a9a8be36950b1c365aeef93d7eb49466f
ZazenX <tnielse21@gmail.com>
2025-01-19 22:57:59 +0100

    Maneuvers with the holy trait pattern

--- src/SatelliteToolboxManeuvers.jl
+++ src/SatelliteToolboxManeuvers.jl
@@ -6,6 +6,26 @@ using Reexport
 @reexport using Dates
 @reexport using SatelliteToolboxBase
 
+
+abstract type Maneuver end
+
+abstract type TransferManeuver <: Maneuver end
+abstract type MaintainanceManeuver <: Maneuver end
+
+
+struct HohmannTransfer <: TransferManeuver
+    orbitᵢ::KeplerianElements
+    orbitₖ::KeplerianElements
+end
+
+
+struct BielipticTransfer <: TransferManeuver
+    orbitᵢ::KeplerianElements
+    orbitₖ::KeplerianElements
+end
+
+
+maneuver(type::Symbol, args...; kwargs...) = maneuver(Val(type), args...; kwargs...)
 include("constants.jl")
 include("types.jl")
 include("maneuvers/hohmanntransfer.jl")
--- src/maneuvers/hohmanntransfer.jl
+++ src/maneuvers/hohmanntransfer.jl
@@ -1,11 +1,23 @@
 export maneuver
 
 """
-Makes a rocket go fast twice tho
+    maneuver(:hohmann, orb_i, orb_k)
+
+Computes the ideal hohmann transfer based on the target orbit orb_k and the initial
+orbit orb_i. Returns a tuple of two changes in absolute velocity. Positive values for 
+flight angle 0°, negative for flight angle 180°
 """
-function maneuver(burn_type::T) where {T<:AbstractManeuver}
-    print("Rocket go fast, wosh!")
+function maneuver(::Val{:hohmann}, orbᵢ::KeplerianElements, orbₖ::KeplerianElements)::NTuple{2, Float64}
+    rᵢ = orbᵢ.a
+    rₖ = orbₖ.a
+
+    Δvᵢ = 10.0
+    Δvₖ = 20.0
+    print("Here's your orbit dude!" * "\n")
+
+    return Δvᵢ, Δvₖ
 end
+    
 
 function maneuver()
     print("Hello from base maneuver")
